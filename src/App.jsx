69f69fb5c531ae83a1e85ffc638c35700aeda4c9
Dave Sproson <dave.sproson@ncas.ac.uk>
2022-12-16 10:39:08 +0000

    Add Suspense. Doh.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import 'bulma/css/bulma.min.css';
 
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom'
 
 import { useDispatchParameters } from './hooks'
@@ -22,14 +22,14 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Navbar />} >
-          <Route path="/" element={<ParameterTable />} />
-          <Route path="/options" element={<Options />} />
-          <Route path="/timeframe" element={<TimeframeSelector />} />
-          <Route path="/config-view" element={<ViewConfig />} />
+          <Route path="/" element={<Suspense><ParameterTable /></Suspense>} />
+          <Route path="/options" element={<Suspense><Options /></Suspense>} />
+          <Route path="/timeframe" element={<Suspense><TimeframeSelector /></Suspense>} />
+          <Route path="/config-view" element={<Suspense><ViewConfig /></Suspense>} />
         </Route>
-        <Route path="/plot" element={<Plot />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tephigram" element={<Tephigram />} />
+        <Route path="/plot" element={<Suspense><Plot /></Suspense>} />
+        <Route path="/dashboard" element={<Suspense><Dashboard /></Suspense>} />
+        <Route path="/tephigram" element={<Suspense><Tephigram /></Suspense>} />
         
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
