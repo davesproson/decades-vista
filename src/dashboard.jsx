1a74429ac1623af3f090b552e844009eec998eaf
Dave Sproson <dave.sproson@ncas.ac.uk>
2022-12-19 17:50:55 +0000

    Add compact=true dashboard option.

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { getData } from "./plotUtils";
 import { useDispatchParameters, useGetParameters } from "./hooks";
 
-const DashPanel = (props) => {
+const LargeDashPanel = (props) => {
     let dataVal = props?.value?.filter(x=>x != null)?.reverse()[0]?.toFixed(2)
 
 
@@ -29,6 +29,38 @@ const DashPanel = (props) => {
     )
 }
 
+const SmallDashPanel = (props) => {
+    let dataVal = props?.value?.filter(x=>x != null)?.reverse()[0]?.toFixed(2)
+
+    return (
+        <div className="m-1 is-flex is-justify-content-center is-flex-grow-1" style={{
+            border: "1px solid black",
+            borderRadius: "5px"
+        }}>
+            <div className="is-flex is-flex-direction-row is-flex-grow-1" >
+                <h3 className="is-uppercase is-justify-content-center is-flex-grow-1 is-flex is-size-7" style={{
+                    // background: "#252243",
+                    // color: "#0abbef",
+                    // borderBottom: "1px solid black"
+                }}>
+                    {props.param.DisplayText}
+                </h3>
+                <span className="is-flex is-justify-content-center is-size-7 is-flex-grow-1">
+                    {dataVal} {props.param.DisplayUnits}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+const DashPanel = (props) => {
+    if(props.size == "large") {
+        return <LargeDashPanel {...props} />
+    } else {
+        return <SmallDashPanel {...props} />
+    }
+}
+
 
 
 const Dashboard = () => {
@@ -36,6 +68,8 @@ const Dashboard = () => {
     const [searchParams, _] = useSearchParams();
     const availableParams = useGetParameters()
     const parameters = searchParams.get("params").split(",")
+    const isCompact = searchParams.get("compact") == "true"
+    const size = isCompact ? "small" : "large"
     const [data, setData] = useState({})
 
     useEffect(() => {
@@ -59,7 +93,10 @@ const Dashboard = () => {
 
     return (
         <div className="is-flex is-flex-wrap-wrap is-justify-content-center">
-            {filteredParams.map(x => <DashPanel key={x.ParameterName} param={x} value={data[x.ParameterName]}/>)}
+            {filteredParams.map(x => <DashPanel size={size} 
+                                                key={x.ParameterName}
+                                                param={x}
+                                                value={data[x.ParameterName]}/>)}
         </div>
   
     )
