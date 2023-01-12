af47ef61b909bd5f15091551fc1c8c13a120f1d1
Dave Sproson <dave.sproson@ncas.ac.uk>
2023-01-12 12:40:49 +0000

    Add a vertical profile view to the library.

--- src/views/libraryEntries.js
+++ src/views/libraryEntries.js
@@ -18,7 +18,9 @@ const libraryViews = [
     }, 
     {
         title: "Basic Meteorology",
-        description: `2x2 grid of basic met parameters`,
+        description: `2x2 grid of basic met parameters, including true air temperatures, dew point
+                      temperatures and humidity, wind speed and direction, and turbulence probe 
+                      differential pressures.`,
         config: {
             nRows: 2,
             nCols: 2,
@@ -27,6 +29,19 @@ const libraryViews = [
                 `${base}plot?timeframe=30min&params=aerack01_buck_mirr_temp,dew_point,specific_humidity&axis=aerack01_buck_mirr_temp,dew_point&axis=specific_humidity&swapxy=false&scrolling=true&data_header=false&style=line&ordvar=utc_time&server=www.faam.ac.uk`,
                 `${base}plot?timeframe=30min&params=gin_eastwards_wind_component,gin_northwards_wind_component,gin_wind_speed&axis=gin_eastwards_wind_component,gin_northwards_wind_component&axis=gin_wind_speed&swapxy=false&scrolling=true&data_header=false&style=line&ordvar=utc_time&server=www.faam.ac.uk`,
                 `${base}plot?timeframe=30min&params=turb_probe_attack_diff,turb_probe_pitot_static,turb_probe_sideslip_diff&axis=turb_probe_attack_diff,turb_probe_sideslip_diff&axis=turb_probe_pitot_static&swapxy=false&scrolling=true&data_header=false&style=line&ordvar=utc_time&server=www.faam.ac.uk`,
+            ]
+        }
+    },
+    {
+        title: "Vertical profiles",
+        description: `A tephigram on the left, and vertical profiles of wind speed and direction,
+                      bulk water (Nevzorov), and aerosol (CPC) on the right.`,
+        config: {
+            nRows: 1,
+            nCols: 2,
+            plots: [
+                `${base}tephigram?timeframe=30min&params=deiced_true_air_temp_c,dew_point&server=www.faam.ac.uk`,
+                `${base}view?nRows=1&nCols=3&plot=aHR0cDovL2xvY2FsaG9zdDo1MTczL2RlY2FkZXMtZGVtby9wbG90P3RpbWVmcmFtZT0zMG1pbiZwYXJhbXM9Z2luX3dpbmRfYW5nbGUsZ2luX3dpbmRfc3BlZWQmYXhpcz1naW5fd2luZF9zcGVlZCZheGlzPWdpbl93aW5kX2FuZ2xlJnN3YXB4eT10cnVlJnNjcm9sbGluZz1mYWxzZSZkYXRhX2hlYWRlcj1mYWxzZSZzdHlsZT1saW5lJm9yZHZhcj1wcmVzc3VyZV9oZWlnaHRfa2Z0JnNlcnZlcj13d3cuZmFhbS5hYy51aw==&plot=aHR0cDovL2xvY2FsaG9zdDo1MTczL2RlY2FkZXMtZGVtby9wbG90P3RpbWVmcmFtZT0zMG1pbiZwYXJhbXM9bmV2em9yb3ZfbGlxdWlkX3dhdGVyLG5ldnpvcm92X3RvdGFsX3dhdGVyJmF4aXM9bmV2em9yb3ZfbGlxdWlkX3dhdGVyLG5ldnpvcm92X3RvdGFsX3dhdGVyJnN3YXB4eT10cnVlJnNjcm9sbGluZz1mYWxzZSZkYXRhX2hlYWRlcj1mYWxzZSZzdHlsZT1saW5lJm9yZHZhcj1wcmVzc3VyZV9oZWlnaHRfa2Z0JnNlcnZlcj13d3cuZmFhbS5hYy51aw==&plot=aHR0cDovL2xvY2FsaG9zdDo1MTczL2RlY2FkZXMtZGVtby9wbG90P3RpbWVmcmFtZT0zMG1pbiZwYXJhbXM9Y3BjMzc4MDFfY291bnRzJmF4aXM9Y3BjMzc4MDFfY291bnRzJnN3YXB4eT10cnVlJnNjcm9sbGluZz1mYWxzZSZkYXRhX2hlYWRlcj1mYWxzZSZzdHlsZT1saW5lJm9yZHZhcj1wcmVzc3VyZV9oZWlnaHRfa2Z0JnNlcnZlcj13d3cuZmFhbS5hYy51aw==`
             ]
         }
     }
