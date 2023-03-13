a6abd46a1ac580caee5709f24a31c9821feb6ca9
Dave Sproson <dave.sproson@ncas.ac.uk>
2023-03-13 14:02:51 +0000

    Allow specification of view width and height.

--- src/views/view.jsx
+++ src/views/view.jsx
@@ -14,12 +14,16 @@ const View = () => {
     const nRows = parseInt(searchParams.get('nRows')) || urls.length
     const nCols = parseInt(searchParams.get('nCols')) || 1
 
-    const width = `${99 / nCols}%`
-    const height = `${99 / nRows}%`
+    // const width = `${99 / nCols}%`
+    // const height = `${99 / nRows}%`
 
     return (
         <div style={{ top: 0, left: 0, width: '100%', height: '100%', position: 'absolute' }}>
             {urls.map((url, i) => {
+                const opts = new URLSearchParams(url.split('?')[1])
+                const width = `${(opts.get('viewWidth') || 100 / nCols) - 1}%`
+                const height = `${(opts.get('viewHeight') || 100 / nRows) - 1}%`
+                console.log(url)
                 return (
                     <iframe key={i} src={url}  width={width} frameBorder="0" scrolling="no"
                         height={height} style={{border: "none", overflow: "hidden"}}/>
