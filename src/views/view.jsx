23e9eb02b7c2bb2276ef36bae89ce5cdb594b28c
Dave Sproson <dave.sproson@ncas.ac.uk>
2023-03-13 14:29:00 +0000

    Allow views to contain plots with url=#. This ignores that plot.

--- src/views/view.jsx
+++ src/views/view.jsx
@@ -5,7 +5,7 @@ import { base } from '../settings'
 const View = () => {
     const [searchParams, _] = useSearchParams();
     const encodedUrls = searchParams.getAll('plot')
-    const urls = encodedUrls.map(url => decode(url)).map(x=>{
+    const urls = encodedUrls.map(url => decode(url)).filter(x=>x!=="#").map(x=>{
         if (x.startsWith('http') || x.startsWith(base)) {
             return x
         }
@@ -14,16 +14,13 @@ const View = () => {
     const nRows = parseInt(searchParams.get('nRows')) || urls.length
     const nCols = parseInt(searchParams.get('nCols')) || 1
 
-    // const width = `${99 / nCols}%`
-    // const height = `${99 / nRows}%`
-
     return (
         <div style={{ top: 0, left: 0, width: '100%', height: '100%', position: 'absolute' }}>
             {urls.map((url, i) => {
                 const opts = new URLSearchParams(url.split('?')[1])
-                const width = `${(opts.get('viewWidth') || 100 / nCols) - 1}%`
-                const height = `${(opts.get('viewHeight') || 100 / nRows) - 1}%`
-                console.log(url)
+                const width = `${(opts.get('viewWidth') || 100 / nCols) - 1 / nCols}%`
+                const height = `${(opts.get('viewHeight') || 100 / nRows) - 1 / nRows}%`
+                
                 return (
                     <iframe key={i} src={url}  width={width} frameBorder="0" scrolling="no"
                         height={height} style={{border: "none", overflow: "hidden"}}/>
